a8e9c501282bbf4cf327de90efde1c0a462a706f
jtaren <arenjatau@gmail.com>
2025-03-02 23:42:10 +0100

    views

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
+from django.utils import timezone
 from django.db.models import F
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.view import generic
+from django.views import generic
 
 from .models import Question, Choice
 
@@ -12,8 +13,9 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        """Return the last five published questions(not including those set to be published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
     model = Question
@@ -23,10 +25,22 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = "polls/results.html"
 
+# def index(request):
+#     latest_question_list = Question.objects.order_by("-pub_date")[:5]
+#     context = {
+#             "latest_question_list": latest_question_list,
+#             }
+#     return render(request, "polls/index.html", context)
+
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {"question": question})
+
 def vote(request, question_id):
-    question = get_object_or_404(question, pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_ser.get(pk=request.POST["choice"])
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(
@@ -44,9 +58,6 @@ def vote(request, question_id):
         # POST data. This prevents data from being posted twice if user hits the Back button.
         return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {
-            "latest_question_list": latest_question_list,
-            }
-    return render(request, "polls/index.html", context)
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {"question": question})
